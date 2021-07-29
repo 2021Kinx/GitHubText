482963dcb34b5797454bc4e41b83aea63cc0c730
京龙 何 <13786092674@163.com>
2021-07-29 10:42:50 +0800

    添加项目文件。

---
+++ GitHubText/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace GitHubText
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine( "这是第一步");
+            Console.WriteLine("这是第二步");
+        }
+    }
+}
